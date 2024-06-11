c34be3ff39bed2e575a862a48ef1c6fe06ee0bfc
Karsten Castillo <castillokarsten@gmail.com>
2024-06-10 22:52:37 -0500

    Adding decision node class

--- decision_tree.py
+++ decision_tree.py
@@ -3,6 +3,14 @@ import pandas as pd
 from gini_index_calculator import GiniCalculator
 
 
+class DecisionNode:
+
+    def __init__(self, criteria_key, choose_left_threshold_value, left_child, right_child):
+        self.criteria_key = criteria_key  # the key "path" to the gini index/entropy
+        self.choose_left_threshold_value = choose_left_threshold_value
+        self.left_child, self.right_child = left_child, right_child
+
+
 class DecisionTreeClassifier:
 
     def __init__(self, x_categories, y_category, training_data, use_entropy=False, skip_columns_for_training=None):
